971b80f1351d915df3747d6b1573848f3040dacd
praveenpriyashan <praveenpriyashan@gmail.com>
2024-11-27 23:02:36 +0530

    add a full crud api  of book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BookController {
@@ -23,7 +24,12 @@ export class BookController {
   }
 
   @Put(':id')
-  async createBooksc (@Param(':id')id:string,@Body() book:CreateBookDto): Promise<Book> {
-    return this.bookService.createBook(book);
+  async updateBooks (@Param('id')id:string,@Body() book:UpdateBookDto): Promise<Book> {
+    return this.bookService.updateBook(id,book);
+  }
+
+  @Delete(':id')
+  async deleteBooks (@Param('id')id:string): Promise<Book> {
+    return this.bookService.deleteBookById(id)
   }
 }
