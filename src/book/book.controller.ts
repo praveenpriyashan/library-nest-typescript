243c4280a27dd071f6e5c577cea3c51c4b56a9f5
praveenpriyashan <praveenpriyashan@gmail.com>
2024-11-27 21:55:10 +0530

    add a update,create api

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
 
-@Controller('book')
+@Controller('books')
 export class BookController {
-  constructor(private bookService: BookService) {
-  }
+  constructor(private bookService: BookService) {}
 
   @Get()
   async getAllBooks(): Promise<Book[]> {
     return this.bookService.getAllBooks();
+  }
+
+  @Post()
+  async createBooks(@Body() book:CreateBookDto): Promise<Book> {
+    return this.bookService.createBook(book);
+  }
+
+  @Get(':id')
+  async getBookById(@Param('id')id:string): Promise<Book> {
+    return this.bookService.findById(id)
+  }
+
+  @Put(':id')
+  async createBooksc (@Param(':id')id:string,@Body() book:CreateBookDto): Promise<Book> {
+    return this.bookService.createBook(book);
   }
 }
