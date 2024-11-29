79199ee7ba5afc0f4ff6bf62b9573275abaa9203
praveenpriyashan <praveenpriyashan@gmail.com>
2024-11-29 22:17:54 +0530

    add a query part find using title

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,14 +2,18 @@ import { Injectable, NotFoundException, Param } from '@nestjs/common';
 import { Book } from './schemas/book.schema';
 import mongoose from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { Query } from 'express-serve-static-core';
+
 
 @Injectable()
 export class BookService {
 
-  constructor(@InjectModel(Book.name) private bookModel: mongoose.Model<Book>) {}
+  constructor(@InjectModel(Book.name) private bookModel: mongoose.Model<Book>) {
+  }
 
-  async getAllBooks(): Promise<Book[]> {
-    const books = await this.bookModel.find();
+  async getAllBooks(query: Query): Promise<Book[]> {
+    const keyword = query.keyword ? { title: { $regex: query.keyword, $options: 'i' } } : {};
+    const books = await this.bookModel.find(keyword);
     return books;
   }
 
@@ -19,12 +23,18 @@ export class BookService {
   }
 
   async findById(id: string): Promise<Book> {
+    const isInvalid = mongoose.isValidObjectId(id);
+    if (!isInvalid) {
+      throw new NotFoundException('Invalid id');
+    }
     const book = await this.bookModel.findById(id);
+
     if (!book) {
       throw new NotFoundException('book not found');
     }
     return book;
   }
+
 
   async updateBook(id: string, book: Book): Promise<Book> {
     const res = await this.bookModel.findByIdAndUpdate(id, book, {
@@ -39,3 +49,4 @@ export class BookService {
     return res;
   }
 }
+
